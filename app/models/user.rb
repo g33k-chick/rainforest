986fdf930a74ad2cad4a6ca9fe753c1be4de7f4c
Melissa D'Agruma <mdagruma@gmail.com>
2015-04-21 16:51:47 -0400

    added Associations and review model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	has_many :reviews
+	has_many :products, through: :reviews
 
 	# attr_accessor :password, :password_confirmation
 	#validates :password, confirmation:true
